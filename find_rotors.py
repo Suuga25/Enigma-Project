29c8f9d480a03bf1c7c5fcbc2574a7f5e76f5ce2
Jakub Šmejkal <jakubsm@users.noreply.github.com>
2025-12-13 20:46:20 +0100

    edit

--- find_rotors.py
+++ find_rotors.py
@@ -1,16 +1,11 @@
-
-from EnigmaModel import EnigmaModel
-from EnigmaConstants import ALPHABET
 from Encrypt import encrypt # The encrypt funtion is defined in encrypt.py. It will exist when repository is merged.
 
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 def find_rotors(message:str, cipher:str) -> str:
-    for a in ALPHABET:
-        for b in ALPHABET:
-            for c in ALPHABET:
+    for a in alphabet:
+        for b in alphabet:
+            for c in alphabet:
                 rotors = a + b + c
                 if encrypt(rotors, message)==cipher:
                     return rotors
     return "Not found"
-
-
-
