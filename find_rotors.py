9235ffdda52f5d9f68bd0b33d33234325f92bf14
Jakub Šmejkal <jakubsm@users.noreply.github.com>
2025-12-18 11:25:14 +0100

    adding comments

--- find_rotors.py
+++ find_rotors.py
@@ -1,11 +1,11 @@
-from Encrypt import encrypt # The encrypt funtion is defined in encrypt.py. It will exist when repository is merged.
+from Encrypt import encrypt # The encrypt funtion is defined in encrypt.py
 
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 def find_rotors(message:str, cipher:str) -> str:
     for a in ALPHABET:
-        for b in ALPHABET:
+        for b in ALPHABET:        # Systematically tries every possible rotor setting
             for c in ALPHABET:
                 rotors = a + b + c
-                if encrypt(rotors, message)==cipher:
+                if encrypt(rotors, message)==cipher: # Checks if output of encrypt() matches the cipher
                     return rotors
     return "Not found"
